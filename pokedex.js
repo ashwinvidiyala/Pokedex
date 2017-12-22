e76773e2af7e2676b979757354e23c6a173f0277
“Ashwin <ashwinvidiyala@gmail.com>
2017-12-21 18:35:37 -0600

    Trying to get things to fetch in the correct order

--- pokedex.js
+++ pokedex.js
@@ -1,7 +1,6 @@
 $(document).ready(function (){
   for (var i = 0; i < 151; i++) {
     $('.pokemon').append(`<img id='${i}' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png">`)
-
   }
 
   $('.pokemon').on('click', 'img', function() {
@@ -9,30 +8,23 @@ $(document).ready(function (){
     // console.log(i);
     // var pokemon = "https://pokeapi.co/api/v2/pokemon/" + i;
       name(i);
-      console.log('name');
       image(i);
-      console.log('image');
-      $('.pokedex').append('<h2> Types </h2>');
+      // $('.pokedex').append('<h2> Types </h2>');
       types(i);
-      console.log('types');
-      $('.pokedex').append('<h2> Height </h2>');
+      // $('.pokedex').append('<h2> Height </h2>');
       height(i);
-      console.log('height');
-      $('.pokedex').append('<h2> Weight </h2>');
+      // $('.pokedex').append('<h2> Weight </h2>');
       weight(i);
-      console.log('weight');
   });
 
   function name(i) {
     $.get("https://pokeapi.co/api/v2/pokemon/" + i, function(data) {
       $('.pokedex').append('<h1> ' + data.name + '</h1>');
-      // console.log(data.name);
     }, "json");
   }
 
   function image(i) {
     $('.pokedex').append(`<img id='${i}' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png">`)
-    // console.log('image');
   }
 
   function types(i) {
@@ -47,13 +39,14 @@ $(document).ready(function (){
 
   function height(i) {
     $.get("https://pokeapi.co/api/v2/pokemon/" + i, function(data) {
-      $('.pokedex').apend('<p> ' + data.height + '</p>');
+      $('.pokedex').append('<h2> ' + data.height.key + '</h2>'); //does this print the key?
+      $('.pokedex').append('<p> ' + data.height + '</p>');
     }, "json");
   }
 
   function weight(i) {
     $.get("https://pokeapi.co/api/v2/pokemon/" + i, function(data) {
-      $('.pokedex').apend('<p> ' + data.weight + '</p>');
+      $('.pokedex').append('<p> ' + data.weight + '</p>');
     }, "json");
   }
 
